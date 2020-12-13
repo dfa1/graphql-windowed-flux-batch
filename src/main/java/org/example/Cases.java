9dbae37e6a7b2c9bf51124ff6bd95acd4b468c85
Davide Angelocola <davide.angelocola@gmail.com>
2020-12-13 08:40:42 +0100

    removing complexity

--- src/main/java/org/example/Cases.java
+++ src/main/java/org/example/Cases.java
@@ -6,7 +6,7 @@ import graphql.GraphQL;
 import graphql.execution.SubscriptionExecutionStrategy;
 import graphql.execution.ValueUnboxer;
 import graphql.schema.DataFetcher;
-import graphql.schema.PropertyDataFetcher;
+import graphql.schema.DataFetchingEnvironment;
 import graphql.schema.idl.SchemaGenerator;
 import graphql.schema.idl.SchemaParser;
 import org.dataloader.BatchLoaderEnvironment;
@@ -15,13 +15,8 @@ import org.dataloader.DataLoader;
 import org.dataloader.DataLoaderOptions;
 import org.dataloader.DataLoaderRegistry;
 import org.example.dto.Person;
-import org.reactivestreams.Publisher;
-import reactor.core.publisher.Flux;
 
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 import java.util.stream.IntStream;
@@ -39,36 +34,28 @@ public class Cases {
 			new SchemaParser()
 				.parse(Cases.class.getResourceAsStream("/schema.graphqls"));
 
-		DataFetcher<CompletableFuture<List<Person>>> listDataFetcher =
+		DataFetcher<Stream<Person>> listDataFetcher =
 			dataFetchingEnvironment -> {
 				// replacing Source.getPersonList();
 				// avoiding allocating a big list upfront...
-				Stream<Person> input = IntStream.range(1, 100).boxed().map(Person::new);
-				DataLoader<Integer, Person> dataLoader = dataFetchingEnvironment.getDataLoaderRegistry().getDataLoader(ENRICHMENT_DATA_LOADER);
-				input.forEach(person -> dataLoader.load(person.getId(), person));
-				return dataLoader.dispatch();
+				return IntStream.range(1, 100).boxed().map(Person::new);
 			};
 
-		DataFetcher<Publisher<Person>> fluxDataPublisher =
-			dataFetchingEnvironment ->
-				Source.getPersonFlux();
-
-		DataFetcher<Publisher<List<Person>>> windowedFluxDataPublisher =
-			dataFetchingEnvironment ->
-				Source.getPersonFluxWindowed();
-
 		DataLoaderOptions dataLoaderOptions = DataLoaderOptions.newOptions().setBatchingEnabled(true).setMaxBatchSize(10);
-		DataLoader<Integer, Person> integerPersonDataLoader = DataLoader.newDataLoader(new EnrichmentServiceBatched(enrichmentService), dataLoaderOptions);
+		DataLoader<Integer, String> integerPersonDataLoader = DataLoader.newDataLoader(new EnrichmentServiceBatched(enrichmentService), dataLoaderOptions);
 
 		dataLoaderRegistry.register(ENRICHMENT_DATA_LOADER, integerPersonDataLoader);
 
 		var runtimeWiring = newRuntimeWiring()
 			.type("Query", builder ->
 				builder.dataFetcher("list", listDataFetcher))
-			.type("Subscription", builder ->
-				builder.dataFetcher("flux", fluxDataPublisher))
-			.type("Subscription", builder ->
-				builder.dataFetcher("fluxWindowed", windowedFluxDataPublisher))
+			.type("Person", builder -> builder.dataFetcher("enrichedString", new DataFetcher() {
+				@Override
+				public Object get(DataFetchingEnvironment environment) throws Exception {
+					System.out.println(environment.getField());
+					return null;
+				}
+			}))
 			.build();
 
 		var graphQlSchema = new SchemaGenerator().makeExecutableSchema(typeDefinitionRegistry, runtimeWiring);
@@ -78,7 +65,7 @@ public class Cases {
 				@Override
 				public Object unbox(Object object) {
 					if (object instanceof CompletableFuture) {
-						return ((CompletableFuture<Object>)object).join();
+						return ((CompletableFuture<Object>) object).join();
 					}
 					return object;
 				}
@@ -87,7 +74,7 @@ public class Cases {
 			.build();
 	}
 
-	private static class EnrichmentServiceBatched implements BatchLoaderWithContext<Integer, Person> {
+	private static class EnrichmentServiceBatched implements BatchLoaderWithContext<Integer, String> {
 
 		private final EnrichmentService enrichmentService;
 
@@ -96,26 +83,13 @@ public class Cases {
 		}
 
 		@Override
-		public CompletionStage<List<Person>> load(List<Integer> keys, BatchLoaderEnvironment environment) {
-			// doing the remote call
-			CompletableFuture<List<String>> enrichmentValuesInBulk = enrichmentService.getEnrichmentValuesInBulk(keys);
-			// join the id with the context (person)
-			return enrichmentValuesInBulk.thenApply(enrichmentValues -> {
-				List<Object> keyContextsList = environment.getKeyContextsList();
-				List<Person> result = new ArrayList<>(enrichmentValues.size());
-				for (int i = 0; i < enrichmentValues.size(); i++) {
-					Person person = (Person) keyContextsList.get(i);
-					String enrichedString = enrichmentValues.get(i);
-					person.setEnrichedString(enrichedString);
-					result.add(person);
-				}
-				return result;
-			});
+		public CompletionStage<List<String>> load(List<Integer> keys, BatchLoaderEnvironment environment) {
+			return enrichmentService.getEnrichmentValuesInBulk(keys);
 		}
 	}
 
 	public static ExecutionResult queryingForList(EnrichmentService enrichmentService) {
-		return  getGraphQl(enrichmentService).execute(ExecutionInput.newExecutionInput()
+		return getGraphQl(enrichmentService).execute(ExecutionInput.newExecutionInput()
 			.query("query list {\n" +
 				"    list {\n" +
 				"        id\n" +
@@ -124,44 +98,5 @@ public class Cases {
 				"}")
 			.dataLoaderRegistry(dataLoaderRegistry)
 			.build());
-	}
-
-	public static void subscribingToFlux(EnrichmentService enrichmentService) {
-		var executionResult = getGraphQl(enrichmentService).execute(ExecutionInput.newExecutionInput()
-			.query("subscription sub {\n" +
-				"    flux {\n" +
-				"        id\n" +
-				"        enrichedString\n" +
-				"    }\n" +
-				"}")
-			.dataLoaderRegistry(dataLoaderRegistry)
-			.build());
-
-		Flux.from(executionResult.getData())
-			.collectList()
-			.block();
-		// of course I wouldn't block, but instead return the flux as server-sent events.
-		// again, I don't care about the data, only how many times the enrichmentService was called, which I assert in the test
-	}
-
-	public static Flux<List<Person>> subscribingToWindowedFlux(EnrichmentService enrichmentService) {
-		var executionResult = getGraphQl(enrichmentService).execute(ExecutionInput.newExecutionInput()
-			.query("subscription sub {\n" +
-				"    fluxWindowed {\n" +
-				"        id\n" +
-				"        enrichedString\n" +
-				"    }\n" +
-				"}")
-			.dataLoaderRegistry(dataLoaderRegistry)
-			.build());
-
-		Publisher<ExecutionResult> executionResults = executionResult.getData();
-		var res = Flux.from(executionResults)
-			.map(executionRes -> {
-				Map<String, List<Person>> singleWindowResults = executionRes.getData();
-				List<Person> peopleWindow = singleWindowResults.get("fluxWindowed");
-				return peopleWindow;
-			});
-		return res;
 	}
 }
--- src/test/java/org/example/CaseTest.java
+++ src/test/java/org/example/CaseTest.java
@@ -3,45 +3,19 @@ package org.example;
 import graphql.ExecutionResult;
 import org.junit.jupiter.api.Test;
 
-import java.util.Collection;
-import java.util.stream.Collectors;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CaseTest {
-    @Test
-    public void should_batch_when_queryingForList() {
-        var enrichmentService = new EnrichmentService();
-        ExecutionResult executionResult = Cases.queryingForList(enrichmentService);
+	@Test
+	public void should_batch_when_queryingForList() {
+		var enrichmentService = new EnrichmentService();
+		ExecutionResult executionResult = Cases.queryingForList(enrichmentService);
 
-        // source is 100 persons with batching of 10
-        assertEquals(10, enrichmentService.getNumberOfTimesCalled());
+		System.out.println(executionResult.toSpecification());
 
-        System.out.println(executionResult.toSpecification());
-    }
+		// source is 100 persons with batching of 10
+		assertEquals(10, enrichmentService.getNumberOfTimesCalled());
 
-    @Test
-    public void should_notBatch_when_subscribingToFlux() {
-        var enrichmentService = new EnrichmentService();
-        Cases.subscribingToFlux(enrichmentService);
+	}
 
-        assertEquals(Source.TOTAL_NUMBER_OF_ITEMS_IN_FLUX, enrichmentService.getNumberOfTimesCalled());
-    }
-
-    @Test
-    public void should_batchPerWindow_when_subscribingToWindowedFlux() {
-        var enrichmentService = new EnrichmentService();
-        var resultFluxOfLists = Cases.subscribingToWindowedFlux(enrichmentService);
-
-        var resultList =
-                resultFluxOfLists
-                        .collectList()
-                        .block()
-                        .stream()
-                        .flatMap(Collection::stream)
-                        .collect(Collectors.toList());
-
-        assertEquals(Source.TOTAL_NUMBER_OF_ITEMS_IN_FLUX / Source.NUMBER_OF_ITEMS_PER_WINDOW, enrichmentService.getNumberOfTimesCalled());
-        assertEquals(10, resultList.size());
-    }
 }
